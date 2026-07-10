f5a7fe86eb6321e2ac193023308f4e82a1356756
Hari <hari2koff@gmail.com>
2026-07-10 14:47:43 +0530

    Finished

--- javascript-practice/sentenceMaker.js
+++ javascript-practice/sentenceMaker.js
@@ -4,6 +4,7 @@ we will use variables to store different parts of the story and then output
 the stories to the console.
 */
 
+// First set of adjectives,...,noun2
 
 let adjective = "tiny";
 let noun = "dragon";
@@ -12,5 +13,17 @@ let place = "forest";
 let adjective2 = "sparkly";
 let noun2 = "cookies";
 
-let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was "+ verb +".";
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was "+ verb +".";
+console.log("First story: "+ firstStory);
 
+//second set of adjective...
+
+adjective = "grumpy";
+noun = "wizard";
+verb = "juggle";
+place = "castle";
+adjective2 = "smoky";
+noun2 = "pancakes";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
