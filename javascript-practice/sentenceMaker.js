bb3586e1fa4c414a5d50a1bbef2496037500af37
Hari <hari2koff@gmail.com>
2026-07-10 14:31:18 +0530

    added firststory var

--- javascript-practice/sentenceMaker.js
+++ javascript-practice/sentenceMaker.js
@@ -12,4 +12,5 @@ let place = "forest";
 let adjective2 = "sparkly";
 let noun2 = "cookies";
 
-let 
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was "+ verb +".";
+
