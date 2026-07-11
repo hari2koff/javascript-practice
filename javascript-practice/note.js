8e5c9d824551a95493802f3b457810e93ad14841
Hari <hari2koff@gmail.com>
2026-07-11 14:42:18 +0530

    learing to work with strings in js

---
+++ javascript-practice/note.js
@@ -0,0 +1,6 @@
+let name = "babu";
+let age = " 44";
+
+const textMessage = `my name is ${name} and i am ${age} old !!`;
+
+console.log(textMessage);
--- javascript-practice/teacherChatbot.js
+++ javascript-practice/teacherChatbot.js
@@ -16,3 +16,6 @@ console.log(subject.length);
 
 console.log(`Here is an example of using the length property on the word ${topic}.`);
 console.log(topic.length);
+
+console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+console.log(subject[0]);
