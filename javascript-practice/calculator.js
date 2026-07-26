dd40a2118b8b571efde3575525cbd884b3fcf9ff
Hari <hari2koff@gmail.com>
2026-07-26 15:33:18 +0530

    Calculator added basic math

--- javascript-practice/calculator.js
+++ javascript-practice/calculator.js
@@ -1,4 +1,21 @@
-function addTwoAndSeven(){
-    return 2 + 7
-
+// Sum
+function calculateSum(num1, num2){
+    return num1 + num2 ;
 }
+console.log(calculateSum(2,5));
+console.log(calculateSum(10,10));
+console.log(calculateSum(5,5));
+
+// Subtract
+function calculateDifference(num1,num2){
+    return num1 - num2;
+}
+console.log(calculateDifference(22,5));
+console.log(calculateDifference(12,1));
+console.log(calculateDifference(17,9));
+
+// Multiplication
+function calculateProduct(num1,num2){
+    return num1*num2;
+}
+console.log(calculateProduct(13,5));
