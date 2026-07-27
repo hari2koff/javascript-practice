7f01467e2a143fd529ae29b022fa6ca6f3a00185
Hari <hari2koff@gmail.com>
2026-07-27 15:22:12 +0530

    Email Masker lab finished

--- javascript-practice/emailMasker.js
+++ javascript-practice/emailMasker.js
@@ -1,4 +1,17 @@
-function maskEmail(email){
+// Email masker - C**********L@example.com
+function emailMasker (email) {
+    let maskIndex = email.indexOf("@") - 1;
+    let maskString = email.slice(1,maskIndex);
+    let maskLength = maskString.length;
+    
     
 
+
+    return email.replace(maskString,"*".repeat(maskLength));
+
 }
+
+const email = "google@gmail.com";
+console.log(emailMasker(email));
+const email1 = "hari2koff@gmail.com";
+console.log(emailMasker(email1));
