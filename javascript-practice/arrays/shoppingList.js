fc69d522b5880a50a9d9fbb544512086059503e5
Hari <hari2koff@gmail.com>
2026-08-03 19:53:52 +0530

    In progres got stuck

---
+++ javascript-practice/arrays/shoppingList.js
@@ -0,0 +1,15 @@
+// Building shoping list
+
+console.log("Grocery shopping list");
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+shoppingList.push("Apples");
+
+//function for shopping list msg 
+// got stuck here will resusme soon 
+function getShoppingListMsg (){
+    
+    return `Current Shopping List: ${shoppingList} `
+}
+
