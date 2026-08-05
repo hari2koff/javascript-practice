a441a6ac87095126e4147f1a8e8c08499acb45fc
Hari <hari2koff@gmail.com>
2026-08-05 11:53:45 +0530

    lunch picker on progress

--- javascript-practice/arrays/lunchPicker.js
+++ javascript-practice/arrays/lunchPicker.js
@@ -1,4 +1,29 @@
 // lunch picker
 const lunches = [];
 
-function addLunchToEnd 
+function addLunchToEnd (lunchArray,itemToAdd) {
+    lunchArray.push(itemToAdd);
+    console.log(`${itemToAdd} added to the end of the lunch menu.`);
+    return lunchArray ;
+}
+
+function addLunchToStart (lunchArray,itemToAdd) {
+    lunchArray.unshift(itemToAdd);
+    console.log(`${itemToAdd} added to the start of the lunch menu.`);
+    return lunchArray;
+}
+
+function removeLastLunch (lunchArray) {
+    let itemRemoved = lunchArray[-1];
+    lunchArray.pop();
+    console.log(`${itemRemoved} removed from the end of the lunch menu.`);
+    return lunchArray;
+}
+
+function removeFirstLunch (lunchArray) {
+    let itemRemoved = lunchArray[0];
+    lunchArray.shift();
+    console.log(`${itemRemoved} removed from the start of the lunch menu.`);
+    return lunchArray;
+}
+
