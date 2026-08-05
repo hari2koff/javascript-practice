bf37110024520ae05ee422aa0e84cb4dea073d9f
Hari <hari2koff@gmail.com>
2026-08-05 15:55:17 +0530

    Lunch picker lab

--- javascript-practice/arrays/lunchPicker.js
+++ javascript-practice/arrays/lunchPicker.js
@@ -12,34 +12,46 @@ function addLunchToStart (lunchArray,itemToAdd) {
     console.log(`${itemToAdd} added to the start of the lunch menu.`);
     return lunchArray;
 }
-// issueToFix - add a logic if no luches are in the array.
+
 function removeLastLunch (lunchArray) {
-    let itemRemoved = lunchArray[-1];
-    lunchArray.pop();
-    console.log(`${itemRemoved} removed from the end of the lunch menu.`);
-    return lunchArray;
+    if (lunchArray.length == 0){
+        console.log("No lunches to remove.");
+    }else {
+        const indexOfLast = lunchArray.length
+        let itemRemoved = lunchArray[(indexOfLast-1)];
+        lunchArray.pop();
+        console.log(`${itemRemoved} removed from the end of the lunch menu.`);
+        return lunchArray;
+    }
 }
+    
 
 function removeFirstLunch (lunchArray) {
-    let itemRemoved = lunchArray[0];
-    lunchArray.shift();
-    console.log(`${itemRemoved} removed from the start of the lunch menu.`);
-    return lunchArray;
+    if (lunchArray.length == 0){
+        console.log("No lunches to remove.");
+    }else {
+        let itemRemoved = lunchArray[0];
+        lunchArray.shift();
+        console.log(`${itemRemoved} removed from the start of the lunch menu.`);
+        return lunchArray;
+    }    
 }
 
 function getRandomLunch (lunchArray) {
     if (lunchArray.length == 0){
-        return "No lunches available.";
+        console.log( "No lunches available.");
+    }else {
+        let randomNum =  Math.floor(Math.random()*lunchArray.length );
+        let randomLunch = lunchArray[randomNum];
+        console.log(`Randomly selected lunch: ${randomLunch}`);
     }
-    let randomNum =  Math.floor(Math.random()*lunchArray.length );
-    let randomLunch = lunchArray[randomNum];
-    console.log(`Randomly selected lunch: ${randomLunch}`);
+    
 }
 
 function showLunchMenu (lunchArray) {
     if (lunchArray.length == 0) {
         console.log("The menu is empty.");
     }else {
-        console.log(`Menu items: ${lunchArray}`);
+        console.log("Menu items: " + lunchArray.join(", "));
     }
 }
